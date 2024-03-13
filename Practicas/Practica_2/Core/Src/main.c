93573254277c9a92e96493c652e707ac88a47254
edicar <edicarvajalp@gmail.com>
2024-03-13 08:29:25 -0500

    ADD .gitignore y comentarios de la practica 2

--- Practicas/Practica_2/Core/Src/main.c
+++ Practicas/Practica_2/Core/Src/main.c
@@ -131,6 +131,7 @@ int main(void) {
 	/* USER CODE END 3 */
 }
 
+//Funcion para inicializar variables de delay
 void delayInit(delay_t *delay, tick_t duration) {
 	if (delay == NULL) {
 		Error_Handler();
@@ -140,29 +141,31 @@ void delayInit(delay_t *delay, tick_t duration) {
 	delay->startTime = 0;
 }
 
+//Funcion para monitorear el estado del retardo
 bool_t delayRead(delay_t *delay) {
-	if (delay == NULL) {
+	if (delay == NULL) {                // Se valida que delay se haya inicializado y que sus miembros no sean NULL
 		Error_Handler();
 	}
 
-	tick_t currentTime = (tick_t) HAL_GetTick();
+	tick_t currentTime = (tick_t) HAL_GetTick(); //Se obtiene la marca de tiempo
 
-	if (delay->running == false) {
+	if (delay->running == false) {               //Si el retarno no está en ejecución se inicia el retardo
 		delay->startTime = currentTime;
 		delay->running = true;
 		return false;
 	} else {
 		tick_t timeElapsed = currentTime - delay->startTime;
 
-		if (timeElapsed >= delay->duration) {
+		if (timeElapsed >= delay->duration) {    // Se implementan acciones cuando el retardo se cumple:
 			delay->running = false;
 			return true;
 		} else {
-			return false;
+			return false;                       // no se ha cumplido se retardo
 		}
 	}
 }
 
+//Funcion para asignar un nuevo retardo
 void delayWrite(delay_t *delay, tick_t duration) {
 	if (delay == NULL) {
 		Error_Handler();
